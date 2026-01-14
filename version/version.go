4bbb86753fbcf059425f300baccdff3ce3a5db88
Nasareen Yaligar <nasareen@Nasareens-MacBook-Pro.local>
2026-01-14 11:34:01 +0530

    IND-6310 - CRT Onboarding

---
+++ version/version.go
@@ -0,0 +1,13 @@
+// Copyright IBM Corp. 2018, 2025
+// SPDX-License-Identifier: MPL-2.0
+
+package version
+
+import "github.com/hashicorp/packer-plugin-sdk/version"
+
+var (
+	Version           = "1.8.1"
+	VersionPrerelease = "dev"
+	VersionMetadata   = ""
+	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
+)
